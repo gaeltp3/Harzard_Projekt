8d82eb87e4a0d08fac10259ffae0751e24073878
Jonny007-MKD <1-23-4-5@web.de>
2013-11-14 14:53:15 +0100

    Changed header of Cell: HasHazard() -> GetHazards()
    Da wir mit unserem Ergebnis ja arbeiten wollen, muss die
    Hazard-Finde-Funktion zurückgeben, wo ein Hazard aufgetreten ist. Da
    jedoch um jede Zelle herum mehrere Hazards auftreten können, muss der
    Rückgabewert ein vector sein.

--- Hazard/Hazard/Cell.h
+++ Hazard/Hazard/Cell.h
@@ -10,11 +10,12 @@ using namespace std;
 class Cell {
 public:
 	bool value;
+	unsigned int index;
 
-	vector<Cell> GetNeighbours();
-	bool HasHazard();
+	vector<Cell*> GetNeighbors();			// returns numElements Cells
+	vector<Cell*> GetHazards();				// returns the neighbor Cells which are hazardous
 
-	Cell(int index, PrimImplikantCollection* &globalPIC)
+	Cell(unsigned int index, PrimImplikantCollection* &globalPIC)
 	{
 		this->index = index;
 		this->primImplikanten = globalPIC->primImplikantenAt(index);
@@ -23,7 +24,6 @@ public:
 
 private:
 	PrimImplikantCollection primImplikanten;
-	int index;
 };
 
 #endif
