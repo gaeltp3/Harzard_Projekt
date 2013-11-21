fabe226ce63a3de0c114063d9e021ec25973a0b8
Jonny007-MKD <1-23-4-5@web.de>
2013-11-21 20:56:54 +0100

    Added stubs of Cell methods

---
+++ Hazard/Hazard/Cell.cpp
@@ -0,0 +1,17 @@
+#include "stdafx.h"
+#include <string>
+#include <vector>
+#include "PrimImplikantCollection.h"
+#include "Cell.h"
+
+vector<Cell*> Cell::GetNeighbors()
+{
+	vector<Cell*> neighbors;
+	return neighbors;
+}
+
+vector<Cell*> Cell::GetHazards()
+{
+	vector<Cell*> neighbors;
+	return neighbors;
+}
