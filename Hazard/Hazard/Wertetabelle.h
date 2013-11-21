1d9e56c7db2ceb7a8c632fa2d87536427ecb7902
gaeltp3 <gaeltp3@hotmail.com>
2013-11-21 22:01:36 +0100

    Headerdatei Wertetabelle :Ausgabe der Werte

---
+++ Hazard/Hazard/Wertetabelle.h
@@ -0,0 +1,43 @@
+#include <string>
+#include <vector>
+#include "PrimImplikantCollection.h"
+#include "Cparser.h"
+#include <iostream>
+
+using namespace std;
+
+unsigned int numElements ;
+bool KNF ;
+
+#ifndef WERTETABELLE
+#define WERTETABELLE
+
+
+class Wertetabelle{
+
+
+public:
+	
+	int print(PrimImplikantCollection* &PIC){
+
+		cout << "KNF=" << KNF << endl;
+		cout << "Dezimalwerte\t|\tAusgangWerte(y)" << endl;
+		for (unsigned int i = 0; i < numElements; i++){
+			cout << "\t" << i << "\t\t|\t" << PIC->valueAt(i) << endl;
+		}
+	}
+
+
+	Wertetabelle(PrimImplikantCollection* &PIC){  // Konstruktor
+		print(PIC);
+	}
+
+};
+
+
+
+
+
+
+
+#endif
